782789fb6d12d9eb2f56fe7dca539a233be4e8e0
peter1891 <peter.baan@outlook.com>
2025-01-01 18:34:48 +0100

    Using a separate library specifically for the FormBuilders.

---
+++ Finance.Forms/TransactionFormBuilder.cs
@@ -0,0 +1,55 @@
+﻿using Finance.Core;
+using Finance.Services.Navigation.Interface;
+using Finance.Utilities.FormBuilder;
+using Finance.Utilities.FormBuilder.Interface;
+using Finance.ViewModels;
+using System.Windows.Controls;
+
+namespace Finance.Forms
+{
+    public class TransactionFormBuilder : IFormBuilder
+    {
+        private readonly INavigationService _navigationService;
+
+        private Form _form;
+
+        public TransactionFormBuilder(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            _form = new Form();
+
+            BuildForm();
+            BuildSubmitButton();
+            BuildCancelButton();
+        }
+
+        public void BuildCancelButton()
+        {
+             RelayCommand cancelRelayCommand = new RelayCommand(obj => { _navigationService.NavigateTo<TransactionsViewModel>(); }, obj => true);
+
+            _form.SetCancelButton("Cancel");
+            _form.SetCancelCommand(cancelRelayCommand);
+        }
+
+        public void BuildForm()
+        {
+            Grid grid = new Grid();
+
+            _form.SetGrid(grid);
+        }
+
+        public void BuildSubmitButton()
+        {
+            RelayCommand submitRelayCommand = new RelayCommand(obj => { _navigationService.NavigateTo<TransactionsViewModel>(); }, obj => true);
+
+            _form.SetSubmitButton("Submit");
+            _form.SetSubmitCommand(submitRelayCommand);
+        }
+
+        public Form GetForm()
+        {
+            return _form;
+        }
+    }
+}
