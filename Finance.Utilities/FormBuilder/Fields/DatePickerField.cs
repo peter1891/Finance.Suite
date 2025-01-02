87f09711c00b035bb1b872d70af7bff4d301e28b
peter1891 <peter.baan@outlook.com>
2025-01-02 22:00:01 +0100

    Adding binding

--- Finance.Utilities/FormBuilder/Fields/DatePickerField.cs
+++ Finance.Utilities/FormBuilder/Fields/DatePickerField.cs
@@ -1,11 +1,14 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace Finance.Utilities.FormBuilder.Fields
 {
     public class DatePickerField : DatePicker
     {
-        public DatePickerField(int row)
+        public DatePickerField(string binding, int row)
         {
+            this.SetBinding(DatePicker.SelectedDateProperty, new Binding(binding));
+
             Grid.SetColumn(this, 1);
             Grid.SetRow(this, row);
         }
