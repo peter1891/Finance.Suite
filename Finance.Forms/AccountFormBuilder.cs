ace3f9525c02a1888c051869a2c406cebc4c34aa
peter1891 <peter.baan@outlook.com>
2025-01-02 08:18:00 +0100

    Setting up a base for building the form using the FormBuilderBase.

---
+++ Finance.Forms/AccountFormBuilder.cs
@@ -0,0 +1,77 @@
+﻿using Finance.Core;
+using Finance.Services.Navigation.Interface;
+using Finance.Utilities.FormBuilder;
+using Finance.Utilities.FormBuilder.Base;
+using Finance.Utilities.FormBuilder.Interface;
+using Finance.ViewModels;
+using System.Windows.Controls;
+
+namespace Finance.Forms
+{
+    public class AccountFormBuilder : FormBuilderBase, IFormBuilder
+    {
+        private readonly INavigationService _navigationService;
+
+        private Form _form;
+
+        public string AccountNumber { get; set; }
+        public string Owner { get; set; }
+
+        public AccountFormBuilder(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+
+            _form = new Form();
+
+            BuildTitle();
+            BuildForm();
+            BuildSubmitButton();
+            BuildCancelButton();
+        }
+
+        public void BuildCancelButton()
+        {
+            RelayCommand cancelRelayCommand = new RelayCommand(obj => { _navigationService.NavigateTo<AccountsViewModel>(); }, obj => true);
+
+            _form.SetCancelButton("Cancel");
+            _form.SetCancelCommand(cancelRelayCommand);
+        }
+
+        public void BuildForm()
+        {
+            Grid grid = BuildGrid(this, 2);
+
+            TextBlock accountNumberBlock = BuildTextBlockField("AccountNumber", 0);
+            grid.Children.Add(accountNumberBlock);
+
+            TextBox accountNumberBox = BuildTextBoxField("AccountNumber", 0);
+            grid.Children.Add(accountNumberBox);
+
+            TextBlock ownerBlock = BuildTextBlockField("Owner", 1);
+            grid.Children.Add(ownerBlock);
+
+            TextBox ownerBox = BuildTextBoxField("Owner", 1);
+            grid.Children.Add(ownerBox);
+
+            _form.SetGrid(grid);
+        }
+
+        public void BuildSubmitButton()
+        {
+            RelayCommand submitRelayCommand = new RelayCommand(obj => { _navigationService.NavigateTo<AccountsViewModel>(); }, obj => true);
+
+            _form.SetSubmitButton("Submit");
+            _form.SetSubmitCommand(submitRelayCommand);
+        }
+
+        public void BuildTitle()
+        {
+            _form.SetTitle("Add Account");
+        }
+
+        public Form GetForm()
+        {
+            return _form;
+        }
+    }
+}
--- Finance.Forms/TransactionFormBuilder.cs
+++ Finance.Forms/TransactionFormBuilder.cs
@@ -1,24 +1,35 @@
 ﻿using Finance.Core;
+using Finance.Repository.Interface.Models;
 using Finance.Services.Navigation.Interface;
 using Finance.Utilities.FormBuilder;
+using Finance.Utilities.FormBuilder.Base;
 using Finance.Utilities.FormBuilder.Interface;
 using Finance.ViewModels;
 using System.Windows.Controls;
 
 namespace Finance.Forms
 {
-    public class TransactionFormBuilder : IFormBuilder
+    public class TransactionFormBuilder : FormBuilderBase, IFormBuilder
     {
         private readonly INavigationService _navigationService;
+        private readonly ITransactionRepository _transactionRepository;
 
         private Form _form;
 
-        public TransactionFormBuilder(INavigationService navigationService)
+        public DateOnly Date { get; set; }
+        public double Amount { get; set; }
+        public string Type {  get; set; }
+        public string CounterParty { get; set; }
+        public string Description { get; set; }
+
+        public TransactionFormBuilder(INavigationService navigationService, ITransactionRepository transactionRepository)
         {
             _navigationService = navigationService;
+            _transactionRepository = transactionRepository;
 
             _form = new Form();
 
+            BuildTitle();
             BuildForm();
             BuildSubmitButton();
             BuildCancelButton();
@@ -26,7 +37,7 @@ namespace Finance.Forms
 
         public void BuildCancelButton()
         {
-             RelayCommand cancelRelayCommand = new RelayCommand(obj => { _navigationService.NavigateTo<TransactionsViewModel>(); }, obj => true);
+            RelayCommand cancelRelayCommand = new RelayCommand(obj => { _navigationService.NavigateTo<TransactionsViewModel>(); }, obj => true);
 
             _form.SetCancelButton("Cancel");
             _form.SetCancelCommand(cancelRelayCommand);
@@ -34,17 +45,47 @@ namespace Finance.Forms
 
         public void BuildForm()
         {
-            Grid grid = new Grid();
+            Grid grid = BuildGrid(this, 3);
+
+            TextBlock dateBlock = BuildTextBlockField("Transaction date", 0);
+            grid.Children.Add(dateBlock);
+
+            DatePicker datePicker = BuildDatePickerField(0);
+            grid.Children.Add(datePicker);
+
+            TextBlock amountBlock = BuildTextBlockField("Amount", 1);
+            grid.Children.Add(amountBlock);
+
+            TextBox amountBox = BuildTextBoxField("Amount", 1);
+            grid.Children.Add(amountBox);
+
+            TextBlock counterPartyBlock = BuildTextBlockField("Counter Party", 2);
+            grid.Children.Add(counterPartyBlock);
+
+            TextBox counterPartyBox = BuildTextBoxField("CounterParty", 2);
+            grid.Children.Add(counterPartyBox);
 
             _form.SetGrid(grid);
         }
 
         public void BuildSubmitButton()
         {
-            RelayCommand submitRelayCommand = new RelayCommand(obj => { _navigationService.NavigateTo<TransactionsViewModel>(); }, obj => true);
+            RelayCommand submitRelayCommand = new RelayCommand(obj => 
+            { 
+                // Data ophalen uit het form
+
+                // Data opslaan in de SQLite DB
+
+                _navigationService.NavigateTo<TransactionsViewModel>(); 
+            }, obj => true);
 
             _form.SetSubmitButton("Submit");
             _form.SetSubmitCommand(submitRelayCommand);
+        }
+
+        public void BuildTitle()
+        {
+            _form.SetTitle("Add Transaction");
         }
 
         public Form GetForm()
