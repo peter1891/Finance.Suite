de178a4f51d022a6202a0efb5911fe8ed7ab4361
peter1891 <peter.baan@outlook.com>
2025-01-08 22:12:49 +0100

    Added methods used for loging in in the application

--- Finance.Repository/Interface/Models/IUserRepository.cs
+++ Finance.Repository/Interface/Models/IUserRepository.cs
@@ -1,8 +1,11 @@
 ﻿using Finance.Models;
+using System.Net;
 
 namespace Finance.Repository.Interface.Models
 {
     public interface IUserRepository : IRepository<UserModel>
     {
+        Task<bool> AuthenticateUserAsync(NetworkCredential networkCredential);
+        Task<Tuple<string, string>> GetUserAsync(NetworkCredential networkCredential);
     }
 }
--- Finance.Repository/Repository/Models/UserRepository.cs
+++ Finance.Repository/Repository/Models/UserRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Finance.Models;
 using Finance.Repository.Interface.Models;
 using Finance.Utilities.Database;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace Finance.Repository.Repository.Models
 {
@@ -10,6 +12,20 @@ namespace Finance.Repository.Repository.Models
             : base(databaseContext)
         {
 
+        }
+
+        public async Task<bool> AuthenticateUserAsync(NetworkCredential networkCredential)
+        {
+            return await DatabaseContext.UserModels
+                .AnyAsync(u => u.Email == networkCredential.UserName && u.Password == networkCredential.Password);
+        }
+
+        public async Task<Tuple<string, string>> GetUserAsync(NetworkCredential networkCredential)
+        {
+            UserModel userModel = await DatabaseContext.UserModels
+                .FirstOrDefaultAsync(u => u.Email == networkCredential.UserName && u.Password == networkCredential.Password);
+
+            return new Tuple<string, string>(userModel.Name, userModel.Email);
         }
 
         public DatabaseContext DatabaseContext
