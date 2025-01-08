812e675007b0e292242a586f87ad5bca1f15f905
peter1891 <peter.baan@outlook.com>
2025-01-08 22:12:19 +0100

    Adding a Finance.CustomControl project

---
+++ Finance.CustomControls/BindablePasswordBox.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Finance.CustomControls
+{
+    public partial class BindablePasswordBox : UserControl
+    {
+        public static readonly DependencyProperty dependencyProperty = DependencyProperty
+            .Register("Password", typeof(SecureString), typeof(BindablePasswordBox));
+
+        public SecureString Password
+        {
+            get { return (SecureString)GetValue(dependencyProperty); }
+            set { SetValue(dependencyProperty, value); }
+        }
+
+        public BindablePasswordBox()
+        {
+            InitializeComponent();
+
+            PasswordBox.PasswordChanged += OnPasswordChanged;
+        }
+
+        private void OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = PasswordBox.SecurePassword;
+        }
+    }
+}
