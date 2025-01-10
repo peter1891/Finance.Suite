82dc921798dbac63f008caefb6eea357cad5a33a
peter1891 <peter.baan@outlook.com>
2025-01-11 00:02:37 +0100

    Adding a PasswordBoxField

---
+++ Finance.Utilities/FormBuilder/Fields/PasswordBoxField.cs
@@ -0,0 +1,23 @@
+﻿using Finance.CustomControls;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Finance.Utilities.FormBuilder.Fields
+{
+    public class PasswordBoxField : BindablePasswordBox
+    {
+        public PasswordBoxField(string binding, int row)
+        {
+            this.Style = (Style)Application.Current.Resources["PasswordBoxField"];
+
+            this.SetBinding(BindablePasswordBox.dependencyProperty, new Binding(binding)
+            {
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
+            });
+
+            Grid.SetColumn(this, 1);
+            Grid.SetRow(this, row);
+        }
+    }
+}
