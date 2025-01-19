1bd5cc31b767a45bdeb80ac4a3d12ee68454d867
peter1891 <peter.baan@outlook.com>
2025-01-19 22:55:14 +0100

    Adding a LabelField for replacing the TextBlockField as a label

---
+++ Finance.Utilities/FormBuilder/Fields/LabelField.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+namespace Finance.Utilities.FormBuilder.Fields
+{
+    public class LabelField : TextBlock
+    {
+        public LabelField(string text, int row)
+        {
+            this.Style = (Style)Application.Current.Resources["LabelField"];
+
+            this.Text = text;
+
+            Grid.SetColumn(this, 0);
+            Grid.SetRow(this, row);
+        }
+    }
+}
